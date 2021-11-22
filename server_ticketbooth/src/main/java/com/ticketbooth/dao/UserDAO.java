dd87aa995a2da2b11bdc58307e504b05a6778527
keshashah <kesha.shah1106@gmail.com>
2021-11-21 20:26:56 -0800

    Added new files

---
+++ server_ticketbooth/src/main/java/com/ticketbooth/dao/UserDAO.java
@@ -0,0 +1,20 @@
+package com.ticketbooth.dao;
+
+import com.ticketbooth.model.User;
+
+import java.util.List;
+
+public interface UserDAO {
+
+    int saveUser(User user);
+
+    int updateUser(User user, int id);
+
+    int deleteUser(int id);
+
+    List<User> getAllUsers();
+
+    User getUserById(int id);
+
+    User getUserByEmail(String email);
+}
---
+++ server_ticketbooth/src/main/java/com/ticketbooth/dao/UserDAOImpl.java
@@ -0,0 +1,54 @@
+package com.ticketbooth.dao;
+
+import com.ticketbooth.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class UserDAOImpl implements UserDAO {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Override
+    public int saveUser(User user) {
+        return jdbcTemplate.update("INSERT INTO user(name, email, gender, dob, country) VALUES (?,?,?,?,?)",
+                new Object[] {user.getName(), user.getEmail(), user.getGender(), user.getDob(), user.getCountry()});
+    }
+
+    @Override
+    public int updateUser(User user, int id) {
+        return jdbcTemplate.update("UPDATE user SET name=?, gender=?, dob=?, country=? WHERE id=?",
+                new Object[]{user.getName(), user.getGender(), user.getDob(), user.getCountry(), id});
+    }
+
+    @Override
+    public int deleteUser(int id) {
+        return jdbcTemplate.update("DELETE FROM user WHERE id=?", id);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return jdbcTemplate.query("SELECT id, name, email, gender, dob, country FROM user",
+                new BeanPropertyRowMapper<>(User.class));
+    }
+
+    @Override
+    public User getUserById(int id) {
+        return jdbcTemplate.queryForObject("SELECT id, name, email, gender, dob, country FROM user WHERE id =?",
+                new BeanPropertyRowMapper<>(User.class),
+                id);
+    }
+
+    @Override
+    public User getUserByEmail(String email) {
+        return jdbcTemplate.queryForObject("SELECT id, name, email, gender, dob, country FROM user WHERE email =?",
+                new BeanPropertyRowMapper<>(User.class),
+                email);
+    }
+
+}
