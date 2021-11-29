466be7bf81ab469b729cca6ebd33166552c581d1
keshashah <kesha.shah1106@gmail.com>
2021-11-28 21:36:02 -0800

    Restructured to move reusable components out og createEventPage

--- client_ticketbooth/src/components/EventPage/CreateEventpage.js
+++ client_ticketbooth/src/components/EventPage/CreateEventpage.js
@@ -1,36 +1,15 @@
 import React from 'react';
 import "./../style.css";
 import Appbar from '../Reusables/Appbar.js';
-import {TextField,Grid, Button} from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
-import Stack from '@mui/material/Stack';
-import DateTimePicker from '@mui/lab/DateTimePicker';
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import ViewEventLayout from './ViewEventLayout.js';
+import {Button} from '@material-ui/core';
 
 const CreateEventPage = () => {
 
-    const onemonthafter = new Date();
-    onemonthafter.setDate(onemonthafter.getDate() + 30)
-    
-
-    const [startvalue, setStartValue] = React.useState(new Date(onemonthafter));
-    const [endvalue, setEndValue] = React.useState(new Date(onemonthafter.setHours(onemonthafter.getHours() +3)));
-
-    const handleStartChange = (newValue) => {
-      setStartValue(newValue);
-      const endValue = new Date(newValue);
-      setEndValue(endValue.setHours(endValue.getHours()+3));
-    };
-
-    const handleEndChange = (newValue) => {
-        setEndValue(newValue);
-      };
-
     const history = useHistory();
 
-    const onCreateEventButtonClick = () => {
-        
+    const onCreateEventButtonClick = () => {     
         history.push('/');
       };
 
@@ -40,50 +19,13 @@ const CreateEventPage = () => {
           
           <div className="topitems">
               <h1>Create New Event Page</h1>
-             <Grid style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column',  alignItems: 'center' }}>  
-                 
-                    <LocalizationProvider dateAdapter={AdapterDateFns}>
-                    <Stack spacing={3}> 
-
-                        <TextField
-                            required
-                            fullWidth
-                            id="outlined-required"
-                            label="Event Name:"
-                            defaultValue=""
-                        />
-                        
-                        <TextField
-                            fullWidth
-                            multiline 
-                            rows={2}
-                            id="outlined"
-                            label="Event Description:"
-                            defaultValue=""
-                        />
-
-                        <DateTimePicker
-                            label="Start Date&Time"
-                            value={startvalue}
-                            onChange={handleStartChange}
-                            renderInput={(params) => <TextField fullWidth {...params} />}
-                        />
-
-                        <DateTimePicker
-                            label="End Date&Time"
-                            value={endvalue}
-                            onChange={handleEndChange}
-                            renderInput={(params) => <TextField  fullWidth {...params} />}
-                        />
-
-                        <Button variant="contained"
-                            onClick={(e) => { onCreateEventButtonClick(e); }}>
-                            Create
-                        </Button>
-
-                      </Stack> 
-                    </LocalizationProvider>
-             </Grid> 
+              <ViewEventLayout />
+              <br/>
+              <br/>
+              <Button variant="contained"
+                       onClick={(e) => { onCreateEventButtonClick(e); }}>
+                        Create
+               </Button>
         </div>
 
       </div>
---
+++ client_ticketbooth/src/components/EventPage/ViewEventLayout.js
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import Stack from '@mui/material/Stack';
+import DateTimePicker from '@mui/lab/DateTimePicker';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import {TextField,Grid} from '@material-ui/core';
+import PropTypes from 'prop-types';
+
+export default function ViewEventLayout(props) {
+
+    const onemonthafter = new Date();
+    onemonthafter.setDate(onemonthafter.getDate() + 30)
+    
+    const [startvalue, setStartValue] = React.useState(new Date(onemonthafter));
+    const [endvalue, setEndValue] = React.useState(new Date(onemonthafter.setHours(onemonthafter.getHours() +3)));
+
+    const handleStartChange = (newValue) => {
+      setStartValue(newValue);
+      const endValue = new Date(newValue);
+      setEndValue(endValue.setHours(endValue.getHours()+3));
+    };
+
+    const handleEndChange = (newValue) => {
+        setEndValue(newValue);
+      };
+
+    return (
+        <div>
+
+        <Grid style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column',  alignItems: 'center' }}>  
+                 
+                    <LocalizationProvider dateAdapter={AdapterDateFns}>
+                    <Stack spacing={3}> 
+
+                        <TextField
+                            required
+                            fullWidth
+                            id="outlined-required"
+                            label="Event Name:"
+                            defaultValue=""
+                        />
+                        
+                        <TextField
+                            fullWidth
+                            multiline 
+                            rows={2}
+                            id="outlined"
+                            label="Event Description:"
+                            defaultValue=""
+                        />
+
+                        <DateTimePicker
+                            label="Start Date&Time"
+                            value={startvalue}
+                            onChange={handleStartChange}
+                            renderInput={(params) => <TextField fullWidth {...params} />}
+                        />
+
+                        <DateTimePicker
+                            label="End Date&Time"
+                            value={endvalue}
+                            onChange={handleEndChange}
+                            renderInput={(params) => <TextField  fullWidth {...params} />}
+                        />
+
+                      </Stack> 
+                    </LocalizationProvider>
+             </Grid> 
+
+
+        </div>
+    );
+};
+ViewEventLayout.propTypes = {
+    // ...prop type definitions here
+    columns: PropTypes.array,
+    rows: PropTypes.array,
+  };
